a90c3860ea862eb8d925a00292c43cbe14567b48
remyCESI <remy.vancamp@viacesi.fr>
2018-09-13 15:32:05 +0200

    verif OP

--- check.js
+++ check.js
@@ -1,54 +1,97 @@
-console.log('test')
-// $('input').keyup( function(){
+ $('input').keyup( function(){
+   console.log('bonjour');
 
-//     console.log('bonjour');
+//Id object
+var IDname = $("#name").attr("id"); 
+var IDfirstName = $("#firstName").attr("id"); 
+var IDclasse = $("#classe").attr("id"); 
+var IDtypeDoc = $("#subject").attr("id"); 
+var IDfichier = $("#fichier").attr("id"); 
 
-// // //Id object
-// // var IDname = $("#name").attr("id"); 
-// // var IDfirstName = $("#firstName").attr("id"); 
-// // var IDclasse = $("#classe").attr("id"); 
-// // var IDtypeDoc = $("#subject").attr("id"); 
-// // var IDfichier = $("#fichier").attr("id"); 
-
-// // //objet 
-// // var name = $("#name"); 
-// // var firstName = $("#firstName");
-// // var classe = $("#classe");
-// // var typeDoc = $("#subject");
-// // var fichier = $("#fichier"); 
+//objet 
+var name = $("#name"); 
+var firstName = $("#first_name");
+var classe = $("#classe");
+var typeDoc = $("#subject");
+var fichier = $("#fichier"); 
 
 
 
-// // //button 
+//button 
 
-// // var boutton = $("button");
+var boutton = $("button");
+
+//param
+
+var check=0;
+
+var filter = /^[a-zA-Z0-9]{3,20}$/;
 
 
+if(filter.test(name.val()) && filter.test(firstName.val()) &&filter.test(typeDoc.val())&& classe.val()!== "Choisir"){
 
-// // var filter = /^[a-zA-Z0-9]{3,20}$/;
+    if($('.alert-success'))
+    {
+        if($('.alert-danger')){$('.alert-danger').remove()}
+        $('.alert-success').remove()
 
+        $('#formDepot').append('<div class="alert alert-success" role="alert"><i style ="margin-left:10px" id="check" class="fas fa-check-square"></i> Succès</div>')
+        
+       check=1
+    }
+    else{
+        $('.alert-success').remove()
 
-// // if(filter.test(name.val()) && filter.test(firstName.val()) &&filter.test(typeDoc.val())&& classe.val()!== "Choisir" && fichier.val()!== ""){
-
-// //     button.append('<i style ="margin-left:10px;" id="check" class="icon fas fa fa-check-circle"></i>')
-
-// //     boutton.removeAttr("disabled");
+        $('#formDepot').append('<div class="alert alert-success" role="alert"><i style ="margin-left:10px" id="check" class="fas fa-check-square"></i>Succès</div>')
+        check=1
+   
+    }
 
 
     
-// // }
-// // else{
+}
+else{
 
-// //     boutton.attr('disabled',"");
+    if($('.alert-danger'))
+    {
+        if($('.alert-succes')){$('.alert-succes').remove()}
+        $('.alert-danger').remove()
+
+        $('#formDepot').append('<div class="alert alert-danger" role="alert"><i style ="margin-left:10px" id="error" class="fas fa-times-circle"></i> Veuillez compléter tous les  champs</div>')
+
+        check=0
+    }
+    else{
+        $('.alert-danger').remove()
+
+        $('#formDepot').append('<div class="alert alert-danger" role="alert"><i style ="margin-left:10px" id="error" class="fas fa-times-circle"></i> Veuillez compléter tous les  champs</div>')
+        check=0;
+   
+    }
+
     
-// //     if($('.icon').attr("id")=="check"){
-// //     $('.icon').remove();
-// //      button.append('<i style ="margin-left:10px id="error" class="fas fa-times-circle"></i>')
-// //     }
 
-// // }
+}
 
 
+if(check=1 && fichier.val() !==""){
+
+    boutton.css('display', 'block'); 
+    boutton.removeAttr('disabled');
+    $('.alert-danger').remove()
+    if($('.alert-success')){
+        $('.alert-success').remove()
+    $('#formDepot').append('<div class="alert alert-success" role="alert"><i style ="margin-left:10px" id="check" class="fas fa-check-square"></i>Succès</div>')
+    }
+
+}
+else{
+    $('.alert-danger').remove()
+
+        $('#formDepot').append('<div class="alert alert-danger" role="alert"><i style ="margin-left:10px" id="error" class="fas fa-times-circle"></i> Veuillez compléter tous les des champs</div>')
     
+}
+   
+console.log(check)
 
-// })
+})
